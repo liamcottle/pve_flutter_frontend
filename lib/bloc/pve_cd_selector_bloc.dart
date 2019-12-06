79fe8194ea0184039f27c8a6c8f6f2ab9b888ef7
Tim Marx <t.marx@proxmox.com>
2019-12-06 17:58:27 +0100

    add cd selector widget
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/bloc/pve_cd_selector_bloc.dart
@@ -0,0 +1,49 @@
+import 'package:pve_flutter_frontend/bloc/proxmox_base_bloc.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_qemu_create_model.dart';
+import 'package:pve_flutter_frontend/states/proxmox_form_field_state.dart';
+
+
+class PveCdSelectorBloc
+    extends ProxmoxBaseBloc<PveCdSelectorEvent, PveCdSelectorState> {
+
+  @override
+  PveCdSelectorState get initialState => PveCdSelectorState(CdType.iso, 'Choose file');
+
+  PveCdSelectorBloc();
+
+  @override
+  Stream<PveCdSelectorState> processEvents(PveCdSelectorEvent event) async* {
+    if (event is ChangeValue) {
+      if (event.value == CdType.iso && (state.value.file == null || state.value.file.isEmpty)) {
+        yield PveCdSelectorState(event.value, "Choose file");
+      } else {
+        yield PveCdSelectorState(event.value, null);
+      }
+    }
+
+    if (event is FileSelected) {
+      yield PveCdSelectorState(state.value.value, null,
+          file: event.volid);
+    }
+  }
+
+
+}
+
+abstract class PveCdSelectorEvent {}
+
+class ChangeValue extends PveCdSelectorEvent {
+  final CdType value;
+  ChangeValue(this.value);
+}
+
+class FileSelected extends PveCdSelectorEvent {
+  final String volid;
+  FileSelected(this.volid);
+}
+
+class PveCdSelectorState extends PveFormFieldState<CdType> {
+  final String file;
+  PveCdSelectorState(CdType value, String error, {this.file})
+      : super(value: value, errorText: error);
+}
---
+++ lib/widgets/pve_cd_selector_widget.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pve_flutter_frontend/bloc/pve_cd_selector_bloc.dart';
+import 'package:pve_flutter_frontend/bloc/pve_file_selector_bloc.dart';
+import 'package:pve_flutter_frontend/bloc/pve_storage_selector_bloc.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_qemu_create_model.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_storage_content_model.dart';
+import 'package:pve_flutter_frontend/widgets/pve_file_selector_widget.dart';
+import 'package:proxmox_dart_api_client/proxmox_dart_api_client.dart'
+    as proxclient;
+
+class PveCdSelector extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final client = Provider.of<proxclient.Client>(context);
+    final cdBloc = Provider.of<PveCdSelectorBloc>(context);
+
+    return StreamBuilder<PveCdSelectorState>(
+        stream: cdBloc.state,
+        initialData: cdBloc.state.value,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final state = snapshot.data;
+            return Column(children: [
+              RadioListTile<CdType>(
+                title: const Text('Use CD/DVD disc image file (iso)'),
+                value: CdType.iso,
+                groupValue: state.value,
+                onChanged: (value) => cdBloc.events.add(ChangeValue(value)),
+              ),
+              if (state.value == CdType.iso)
+                OutlineButton(
+                  borderSide: state.hasError ? BorderSide(color: Colors.red) : null,
+                  child: Text((state.file == null ||state.file.isEmpty) ? "Choose File" : state.file),
+                  onPressed: () async {
+                    final PveNodesStorageContentModel file = await showDialog(
+                        context: context,
+                        builder: (context) => PveFileSelector(
+                              fBloc: PveFileSelectorBloc(
+                                apiClient: client,
+                                fileType: PveStorageContentType.iso,
+                              ),
+                              sBloc: PveStorageSelectorBloc(
+                                  apiClient: client,
+                                  fetchEnabledStoragesOnly: true,
+                                  content: PveStorageContentType.iso)
+                                ..events.add(LoadStoragesEvent()),
+                            ));
+                    if (file != null && file is PveNodesStorageContentModel) {
+                      cdBloc.events.add(FileSelected(file.volid));
+                    }
+                  },
+                ),
+              RadioListTile<CdType>(
+                title: const Text('Use physical CD/DVD Drive'),
+                value: CdType.cdrom,
+                groupValue: state.value,
+                onChanged: (value) => cdBloc.events.add(ChangeValue(value)),
+              ),
+              RadioListTile<CdType>(
+                title: const Text('Do not use any media'),
+                value: CdType.none,
+                groupValue: state.value,
+                onChanged: (value) => cdBloc.events.add(ChangeValue(value)),
+              ),
+            ]);
+          }
+
+          return Container();
+        });
+  }
+}
