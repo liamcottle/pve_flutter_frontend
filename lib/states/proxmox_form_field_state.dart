2e3e48108f6d9f268fca54d327b2a9a46bb11092
Tim Marx <t.marx@proxmox.com>
2019-12-06 17:49:43 +0100

    add custom form field state
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/states/proxmox_form_field_state.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+
+class PveFormFieldState<T> {
+  final T value;
+
+  final String errorText;
+
+  final bool isRequired;
+
+  final bool isValidating;
+
+  final bool enabled;
+
+
+  PveFormFieldState({@required this.value, this.isRequired = false, this.isValidating = false, this.enabled = true, this.errorText});
+
+  /// True if this field has any validation errors.
+  bool get hasError => errorText != null;
+
+
+}
