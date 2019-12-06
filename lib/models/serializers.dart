874c84dddf72af0ca397678aecc08204db3b3d26
Tim Marx <t.marx@proxmox.com>
2019-12-06 17:49:11 +0100

    update serializers
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

--- lib/models/serializers.dart
+++ lib/models/serializers.dart
@@ -2,9 +2,14 @@ import 'package:built_collection/built_collection.dart';
 import 'package:built_value/json_object.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
+import 'package:pve_flutter_frontend/models/pve_bool_serializer.dart';
 import 'package:pve_flutter_frontend/models/pve_cluster_resources_model.dart';
 import 'package:pve_flutter_frontend/models/pve_cluster_tasks_model.dart';
 import 'package:pve_flutter_frontend/models/pve_nodes_model.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_network_model.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_qemu_create_model.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_storage_content_model.dart';
+import 'package:pve_flutter_frontend/models/pve_nodes_storage_model.dart';
 
 
 part 'serializers.g.dart';
@@ -13,6 +18,11 @@ part 'serializers.g.dart';
   PveClusterResourcesModel,
   PveClusterTasksModel,
   PveNodesModel,
+  PveNodesStorageModel,
+  PveNodesStorageContentModel,
+  PveNodeNetworkReadModel,
+  PveNodeQemuCreateModel
 ])
 final Serializers serializers =
-    (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())).build();
+    (_$serializers.toBuilder()..addPlugin(StandardJsonPlugin())..add(PveBoolSerializer())).build();
+
--- lib/models/serializers.g.dart
+++ lib/models/serializers.g.dart
@@ -7,9 +7,19 @@ part of 'serializers.dart';
 // **************************************************************************
 
 Serializers _$serializers = (new Serializers().toBuilder()
+      ..add(Bios.serializer)
+      ..add(BridgeType.serializer)
+      ..add(CdType.serializer)
+      ..add(OSType.serializer)
+      ..add(ProcessorArch.serializer)
       ..add(PveClusterResourcesModel.serializer)
       ..add(PveClusterTasksModel.serializer)
-      ..add(PveNodesModel.serializer))
+      ..add(PveNodeNetworkReadModel.serializer)
+      ..add(PveNodeQemuCreateModel.serializer)
+      ..add(PveNodesModel.serializer)
+      ..add(PveNodesStorageContentModel.serializer)
+      ..add(PveNodesStorageModel.serializer)
+      ..add(ScsiControllerModel.serializer))
     .build();
 
 // ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
