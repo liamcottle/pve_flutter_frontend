a692a20b2c17197a84859d75c7adbffe2c61d2f3
Tim Marx <t.marx@proxmox.com>
2019-12-06 18:08:56 +0100

    add resource overwiew
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

--- lib/pages/main_layout_wide.dart
+++ lib/pages/main_layout_wide.dart
@@ -1,15 +1,47 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:pve_flutter_frontend/bloc/proxmox_global_error_bloc.dart';
 import 'package:pve_flutter_frontend/bloc/pve_task_log_bloc.dart';
 import 'package:pve_flutter_frontend/widgets/pve_main_navigation_drawer.dart';
+import 'package:pve_flutter_frontend/widgets/pve_resource_overview_widget.dart';
 import 'package:pve_flutter_frontend/widgets/pve_task_log_widget.dart';
 import 'package:proxmox_dart_api_client/proxmox_dart_api_client.dart'
     as proxclient;
 
-class MainLayoutWide extends StatelessWidget {
+class MainLayoutWide extends StatefulWidget {
+  @override
+  _MainLayoutWideState createState() => _MainLayoutWideState();
+}
+
+class _MainLayoutWideState extends State<MainLayoutWide> {
+  StreamSubscription _errorsSubscription;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _errorsSubscription ??= ProxmoxGlobalErrorBloc().onError.listen((error) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        content: Text(error.toString()),
+        backgroundColor: Colors.red,
+        behavior: SnackBarBehavior.floating,
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    _errorsSubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final client = Provider.of<proxclient.Client>(context);
+
     return Scaffold(
+        key: _scaffoldKey,
         drawer: PveMainNavigationDrawer(),
         appBar: AppBar(
           title: Text("Proxmox"),
@@ -17,38 +49,35 @@ class MainLayoutWide extends StatelessWidget {
             IconButton(
               icon: Icon(Icons.help),
               tooltip: "Documantation",
-              onPressed: () {
-                // html.window.open(
-                //     "https://pve.proxmox.com/pve-docs/pve-admin-guide.html",
-                //     "Documentation");
-              },
+              onPressed: () {},
             ),
-            LogButton()
+            Provider<PveTaskLogBloc>(
+                builder: (context) => PveTaskLogBloc(apiClient: client)
+                  ..events.add(LoadRecentTasks()),
+                dispose: (context, value) => value.dispose(),
+                child: LogButton())
           ],
         ),
-        body: Container());
+        body: Card(child: PveResourceOverview()));
   }
 }
 
 class LogButton extends StatelessWidget {
-  const LogButton({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    final taskBloc = Provider.of<PveTaskLogBloc>(context);
     return IconButton(
       icon: Icon(Icons.view_list),
-      tooltip: "Cluster logs",
+      tooltip: "Recent Tasks",
       onPressed: () {
         showBottomSheet(
             context: context,
-            builder: (context) => Container(
+            builder: (otherContext) => Container(
                 height: MediaQuery.of(context).size.height * 0.25,
                 width: MediaQuery.of(context).size.width,
                 child: PVETaskLog(
-                    bloc: PveTaskLogBloc(
-                        apiClient: Provider.of<proxclient.Client>(context)))));
+                  bloc: taskBloc,
+                )));
       },
     );
   }
---
+++ lib/widgets/pve_resource_overview_widget.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pve_flutter_frontend/bloc/pve_resource_bloc.dart';
+import 'package:pve_flutter_frontend/models/pve_cluster_resources_model.dart';
+import 'package:pve_flutter_frontend/utils/renderers.dart';
+
+class PveResourceOverview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final rbloc = Provider.of<PveResourceBloc>(context);
+    return StreamBuilder<PveResourceState>(
+        stream: rbloc.state,
+        initialData: rbloc.state.value,
+        builder: (context, snapshot) => SingleChildScrollView(
+              child: DataTable(
+                columns: [
+                  DataColumn(
+                    label: Text("Type"),
+                  ),
+                  DataColumn(
+                    label: Text("Description"),
+                  ),
+                  DataColumn(
+                    label: Text("Node"),
+                  ),
+                  DataColumn(
+                    label: Text("Action"),
+                  ),
+                ],
+                rows: snapshot.data.resources
+                    .where((resource) => resource.type != "pool")
+                    .map((resource) => DataRow(cells: [
+                          DataCell(Renderers.getDefaultResourceIcon(
+                              resource.type, resource.shared, resource.status)),
+                          DataCell(Text(resource.displayName)),
+                          DataCell(Text(resource.node ?? "")),
+                          DataCell(Row(
+                            children: getResourceActions(resource, rbloc),
+                          ))
+                        ]))
+                    .toList(),
+              ),
+            ));
+  }
+
+  List<Widget> getResourceActions(
+      PveClusterResourcesModel resource, PveResourceBloc rbloc) {
+    switch (resource.type) {
+      case "node":
+        if (resource.status == "online") {
+          return [
+            IconButton(
+              icon: Icon(
+                Icons.power_settings_new,
+                color: Colors.red,
+              ),
+              tooltip: "Turn off",
+              onPressed: () => rbloc.events.add(
+                  PerformActionOnResource(ResourceAction.shutdown, resource)),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.autorenew,
+              ),
+              tooltip: "Reboot",
+              onPressed: () => rbloc.events.add(
+                  PerformActionOnResource(ResourceAction.reboot, resource)),
+            ),
+          ];
+        }
+        return [];
+
+      case "qemu":
+        if (resource.status == "running") {
+          return [
+            IconButton(
+              icon: Icon(
+                Icons.power_settings_new,
+                color: Colors.red,
+              ),
+              onPressed: () => rbloc.events.add(
+                  PerformActionOnResource(ResourceAction.shutdown, resource)),
+              tooltip: "Shutdown",
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.power,
+                color: Colors.red,
+              ),
+              tooltip: "Unplug",
+              onPressed: () => rbloc.events
+                  .add(PerformActionOnResource(ResourceAction.stop, resource)),
+            ),
+          ];
+        }
+        return [
+          IconButton(
+            icon: Icon(
+              Icons.power_settings_new,
+              color: Colors.green,
+            ),
+            onPressed: () => rbloc.events
+                .add(PerformActionOnResource(ResourceAction.start, resource)),
+            tooltip: "Start",
+          ),
+        ];
+
+      case "lxc":
+        if (resource.status == "running") {
+          return [
+            IconButton(
+              icon: Icon(
+                Icons.power_settings_new,
+                color: Colors.red,
+              ),
+              onPressed: () => rbloc.events
+                  .add(PerformActionOnResource(ResourceAction.stop, resource)),
+              tooltip: "Shutdown",
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.power,
+                color: Colors.red,
+              ),
+              tooltip: "Unplug",
+              onPressed: () => rbloc.events.add(
+                  PerformActionOnResource(ResourceAction.shutdown, resource)),
+            ),
+          ];
+        }
+        return [
+          IconButton(
+            icon: Icon(
+              Icons.power_settings_new,
+              color: Colors.green,
+            ),
+            tooltip: "Start",
+            onPressed: () => rbloc.events
+                .add(PerformActionOnResource(ResourceAction.start, resource)),
+          )
+        ];
+      case "storage":
+        return [];
+      case "pool":
+        return [];
+      default:
+        return [];
+    }
+  }
+}
