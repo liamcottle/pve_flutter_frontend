3461d1b14b3fd683c984f3d26426103a5f959dd1
Tim Marx <t.marx@proxmox.com>
2019-12-06 17:33:36 +0100

    add global error bloc
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/bloc/proxmox_global_error_bloc.dart
@@ -0,0 +1,17 @@
+import 'package:rxdart/rxdart.dart';
+
+class ProxmoxGlobalErrorBloc {
+  static final ProxmoxGlobalErrorBloc _singleton =
+      ProxmoxGlobalErrorBloc._internal();
+
+  final PublishSubject<Object> _errorEventController =
+      PublishSubject<Object>();
+  Function(Object) get addError => _errorEventController.sink.add;
+  Stream<Object> get onError => _errorEventController.stream;
+
+  factory ProxmoxGlobalErrorBloc() {
+    return _singleton;
+  }
+
+  ProxmoxGlobalErrorBloc._internal();
+}
