c70f7573b7f1ca866abb8b926401e70b88017e80
Tim Marx <t.marx@proxmox.com>
2019-12-06 17:44:04 +0100

    add dns name validator
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -6,11 +6,18 @@ class Validators {
     r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
   );
 
+  static final RegExp _dnsExp = new RegExp(
+        r'^(?:(([a-zA-Z0-9]([a-zA-Z0-9\\-]*[a-zA-Z0-9])?)\\.)*([A-Za-z0-9]([A-Za-z0-9\\-]*[A-Za-z0-9])?))$');
+
   static isValidEmail(String email) {
     return _emailRegExp.hasMatch(email);
   }
 
   static isValidPassword(String password) {
     return _passwordRegExp.hasMatch(password);
+  }
+
+  static isValidDnsName(String name) {
+    return _dnsExp.hasMatch(name);
   }
 }
