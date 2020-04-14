779b4b05b041c5a8652394bb15f8180ace1970d2
Tim Marx <t.marx@proxmox.com>
2020-04-15 01:16:40 +0200

    add ActionCard widget
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/widgets/pve_action_card_widget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ActionCard extends StatelessWidget {
+  final Function onTap;
+  final String title;
+  final Widget icon;
+
+  const ActionCard({
+    Key key,
+    this.onTap,
+    this.title,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0xFF00617F),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 80,
+            child: Stack(children: [
+              Center(
+                child: icon,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Center(
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
