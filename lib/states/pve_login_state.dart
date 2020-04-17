959dc9727a931e9780893a501585ffa49d04ed5a
Tim Marx <t.marx@proxmox.com>
2020-04-17 12:50:56 +0200

    rename login state file and add fields
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/states/pve_login_state.dart
@@ -0,0 +1,41 @@
+import 'package:built_value/built_value.dart';
+import 'package:proxmox_dart_api_client/proxmox_dart_api_client.dart';
+import 'package:pve_flutter_frontend/states/pve_base_state.dart';
+
+part 'pve_login_state.g.dart';
+
+abstract class PveLoginState
+    with PveBaseState
+    implements Built<PveLoginState, PveLoginStateBuilder> {
+  String get userNameFieldError;
+  String get passwordFieldError;
+  String get originFieldError;
+  String get origin;
+
+  bool get isUsernameValid => userNameFieldError.isEmpty;
+  bool get isPasswordValid => passwordFieldError.isEmpty;
+  bool get isOriginValid => originFieldError.isEmpty;
+
+  @nullable
+  ProxmoxApiClient get apiClient;
+
+  bool get isFormValid => isUsernameValid && isPasswordValid && isOriginValid;
+
+  factory PveLoginState([void Function(PveLoginStateBuilder) updates]) =
+      _$PveLoginState;
+  PveLoginState._();
+
+  factory PveLoginState.init(String origin) {
+    return PveLoginState((b) => b
+      //base
+      ..errorMessage = ''
+      ..isBlank = true
+      ..isLoading = false
+      ..isSuccess = false
+      //class
+      ..userNameFieldError = ''
+      ..passwordFieldError = ''
+      ..originFieldError = ''
+      ..origin = origin);
+  }
+}
