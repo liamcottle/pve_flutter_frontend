3a4dfd93c11c6467cf7c71eea99d6ff2de8fb7c0
Tim Marx <t.marx@proxmox.com>
2020-04-20 19:16:03 +0200

    add ipv4 validator
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

--- lib/utils/validators.dart
+++ lib/utils/validators.dart
@@ -6,8 +6,11 @@ class Validators {
     r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
   );
 
-  static final RegExp _dnsExp = new RegExp(
-        r'^(?:(([a-zA-Z0-9]([a-zA-Z0-9\\-]*[a-zA-Z0-9])?)\\.)*([A-Za-z0-9]([A-Za-z0-9\\-]*[A-Za-z0-9])?))$');
+  static final RegExp _dnsExp = RegExp(
+      r'^(?:(([a-zA-Z0-9]([a-zA-Z0-9\\-]*[a-zA-Z0-9])?)\\.)*([A-Za-z0-9]([A-Za-z0-9\\-]*[A-Za-z0-9])?))$');
+  static const ipv4Octet = "(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])";
+  static final RegExp _ipv4RegExp =
+      RegExp("^(?:(?:(?:$ipv4Octet\\.){3}$ipv4Octet))\$");
 
   static isValidEmail(String email) {
     return _emailRegExp.hasMatch(email);
@@ -19,5 +22,9 @@ class Validators {
 
   static isValidDnsName(String name) {
     return _dnsExp.hasMatch(name);
+  }
+
+  static isValidIPV4(String ip) {
+    return _ipv4RegExp.hasMatch(ip);
   }
 }
