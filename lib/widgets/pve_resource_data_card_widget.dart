569215e9dea88d63d4de64d43fd98ad1d12c6de2
Tim Marx <t.marx@proxmox.com>
2020-05-14 14:29:29 +0200

    add PveResourceDataCardWidget for more consistency
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/widgets/pve_resource_data_card_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class PveResourceDataCardWidget extends StatelessWidget {
+  final bool showTitleTrailing;
+  final bool expandable;
+  final Widget title;
+  final Widget titleTrailing;
+  final Widget subtitle;
+  final EdgeInsets padding;
+  final List<Widget> children;
+
+  const PveResourceDataCardWidget({
+    Key key,
+    this.showTitleTrailing = false,
+    this.title,
+    this.titleTrailing,
+    this.subtitle,
+    this.children,
+    this.padding = const EdgeInsets.all(8.0),
+    this.expandable = false,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: Card(
+        color: Colors.white,
+        child: Builder(
+          builder: (context) {
+            if (expandable) {
+              return ExpansionTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    title,
+                    if (showTitleTrailing) titleTrailing,
+                  ],
+                ),
+                subtitle: subtitle,
+                children: children,
+              );
+            }
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      title,
+                      if (showTitleTrailing) titleTrailing,
+                    ],
+                  ),
+                  subtitle: subtitle,
+                ),
+                Divider(
+                  indent: 10,
+                  endIndent: 10,
+                ),
+                ...children
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
