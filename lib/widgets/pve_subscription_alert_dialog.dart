7553e72d8b3d76d2761105fe2406857694458126
Tim Marx <t.marx@proxmox.com>
2020-05-26 15:19:42 +0200

    add PveSubscriptionAlertDialog
    Signed-off-by: Tim Marx <t.marx@proxmox.com>

---
+++ lib/widgets/pve_subscription_alert_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PveSubscriptionAlertDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Subscription'),
+      content: Text('One or more nodes do not have a valid subscription.\n\n'
+          'The Proxmox team works very hard to make sure you are running the best'
+          ' software and getting stable updates and security enhancements,'
+          ' as well as quick enterprise support.\n\nPlease consider to buy a subscription.'),
+      actions: [
+        FlatButton(
+            onPressed: () async {
+              final url = 'https://www.proxmox.com/proxmox-ve/pricing';
+              if (await canLaunch(url)) {
+                return await launch(url);
+              } else {
+                throw 'Could not launch $url';
+              }
+            },
+            child: Text('Get subscription'))
+      ],
+    );
+  }
+}
