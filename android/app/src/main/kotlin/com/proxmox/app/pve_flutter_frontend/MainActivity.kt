e37cd7c6a6dbf597a7bc22177cbb8cb602b7ddec
Thomas Lamprecht <t.lamprecht@proxmox.com>
2021-12-09 07:47:44 +0100

    android: migrate to compiled API v31+, with splash screen support
    use AndroidX for getting compat support with our min SDK level v23
    
    The splash screen support is now handled directly by android, I
    actually noticed that as Flutter complained about the old way
    being used and pointed to
    http://flutter.dev/go/android-splash-migration
    which redirects to
    https://docs.flutter.dev/development/ui/advanced/splash-screen
    
    Note, I saw some issues with the icon not showing up on Android 12
    while the older versions were OK, seems like it could be:
    https://issuetracker.google.com/issues/207095799
    
    But need to closer investigate, for now splash screen is not /that/
    essential and the basics should be right, so we better move forward
    now and bugfix whatever it is.
    
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- android/app/src/main/kotlin/com/proxmox/app/pve_flutter_frontend/MainActivity.kt
+++ android/app/src/main/kotlin/com/proxmox/app/pve_flutter_frontend/MainActivity.kt
@@ -3,12 +3,17 @@ package com.proxmox.app.pve_flutter_frontend
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.annotation.NonNull
+import androidx.core.view.WindowCompat;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import androidx.core.content.FileProvider
 import android.util.Log
 import android.os.Environment;
+import android.os.Bundle;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -43,6 +48,17 @@ class MainActivity: FlutterActivity() {
                 }
             }
         }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+
+        val splashScreen = installSplashScreen()
+
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+
+        splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+    }
 
     fun shareFile(path:String, type:String){
         var file = File(path);
